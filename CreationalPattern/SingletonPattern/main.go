9e3109b498a7600fe64ee067df63e1629657af14
V <lytranvinh.work@gmail.com>
2024-07-15 01:04:40 +0700

    [Upgrade]: singleton parttern cmp

--- CreationalPattern/SingletonPattern/main.go
+++ CreationalPattern/SingletonPattern/main.go
@@ -8,8 +8,47 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
+// Handle concurrency 
+var lock =  &sync.Mutex{}
+
+type single struct {
+	// TODO - Add some properties
+}
+
+// Variable to store single instance
+var singleInstance *single
+
+func getInstance() *single {
+	if singleInstance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if singleInstance == nil {
+			fmt.Println("Creating single instance now.")
+			singleInstance = &single{}
+		} else {
+			fmt.Println("Single instance already created.")
+		}
+	} else {
+		fmt.Println("Single instance already created.")
+	}
+
+	return singleInstance
+}
+
 func main() {
-	fmt.Println("Singleton Pattern")
+	
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < 25; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			getInstance()
+		}()
+    }
+
+	wg.Wait()
 }
