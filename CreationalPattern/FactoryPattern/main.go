a6e671cedeefb955c3960139237ac6a3ea064a6c
V <lytranvinh.work@gmail.com>
2024-07-14 22:24:07 +0700

    [Upgrade]: Format and add struct plant

--- CreationalPattern/FactoryPattern/main.go
+++ CreationalPattern/FactoryPattern/main.go
@@ -1,4 +1,6 @@
-/**
+/*
+*
+
 	@author: Ly Tran Vinh
 	@contact: lytranvinh.work@gmail.com
 	@content: Observer Pattern
@@ -16,27 +18,83 @@ type TypePlant int
  */
 
 const (
-	SUNFLOWER 	TypePlant = iota 
+	SUNFLOWER TypePlant = iota
 	PEASHOOTER
 	WALLNUT
-	REAPEATER
 	// TODO add more plant type ...
 )
 
 // String method for TypePlant type
 func (d TypePlant) String() string {
-    return [...]string{
-		"SunFlower", 
-		"PeaShooter", 
-		"WallNut", 
-		"ReaPeater",
+	return [...]string{
+		"SunFlower",
+		"PeaShooter",
+		"WallNut",
 		// TODO add more plant type ...
 	}[d]
 }
 
+/**
+ * Plant interface
+ */
+type Plant interface {
+	PlantName() string
+	PlantPrice() int
+	// TODO add more method ...
+}
 
+/**
+ * SunFlower struct, implement Plant interface
+ */
+type SunFlower struct {
+	Name  string
+	Price int
+	// TODO add more attribute ...
+}
 
-func main()  {
+func (s *SunFlower) PlantName() string {
+	return s.Name
+}
+
+func (s *SunFlower) PlantPrice() int {
+	return s.Price
+}
+
+/**
+ * PEASHOOTER struct, implement Plant interface
+ */
+type PeaShooter struct {
+	Name  string
+	Price int
+	// TODO add more attribute ...
+}
+
+func (s *PeaShooter) PlantName() string {
+	return s.Name
+}
+
+func (s *PeaShooter) PlantPrice() int {
+	return s.Price
+}
+
+/**
+ * WallNut struct, implement Plant interface
+ */
+type WallNut struct {
+	Name  string
+	Price int
+	// TODO add more attribute ...
+}
+
+func (s *WallNut) PlantName() string {
+	return s.Name
+}
+
+func (s *WallNut) PlantPrice() int {
+	return s.Price
+}
+
+func main() {
 
 	fmt.Println(SUNFLOWER)
 	fmt.Println(PEASHOOTER)
