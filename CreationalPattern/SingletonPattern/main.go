98824049f3463d12106e5106a33252e73b008cca
V <lytranvinh.work@gmail.com>
2024-07-15 00:53:29 +0700

    [ADD]: Loadding file singleton pattern .....

---
+++ CreationalPattern/SingletonPattern/main.go
@@ -0,0 +1,15 @@
+/**
+@author: Ly Tran Vinh
+@contact: lytranvinh.work@gmail.com
+@content: Abstract Singleton Pattern
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Singleton Pattern")
+}
