15de08073491cf45b2c8a7802cb87069de48c840
V <lytranvinh.work@gmail.com>
2024-07-16 01:54:33 +0700

    [ADD]: Loadding file ...

---
+++ CreationalPattern/AbstractFactoryPattern/Java/main.java
@@ -0,0 +1,11 @@
+/**
+ * @author: Ly Tran Vinh
+ * @contact: lytranvinh.work@gmail.com
+ * @content: Abstract Factory Pattern
+ */
+
+public class main {
+    public static void main(String[] args) {
+        
+        return;
+    }
---
+++ CreationalPattern/AbstractFactoryPattern/Python/main.py
@@ -0,0 +1,13 @@
+"""
+    @author: Ly Tran Vinh
+    @contact: lytranvinh.work@gmail.com
+    @content: Abstract Factory Pattern
+"""
+
+def main():
+    """
+        Main function to run the code
+    """
+
+if __name__ == '__main__':
+    main()
