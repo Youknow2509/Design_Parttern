bad1287f5413e280b08dcf88521682f4e63626d9
V <lytranvinh.work@gmail.com>
2024-07-16 00:50:46 +0700

    [ADD]: Add loadding Factory Pattern ...

---
+++ CreationalPattern/FactoryPattern/Python/main.py
@@ -0,0 +1,11 @@
+""" 
+    @author: Ly Tran Vinh
+	@contact: lytranvinh.work@gmail.com
+	@content: Factory Pattern
+"""
+
+def main():
+    print("Factory Pattern")
+    
+if __name__ == '__main__':
+    main()
