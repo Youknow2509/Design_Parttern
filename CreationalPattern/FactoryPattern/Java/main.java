12681fd95c2aeed5657160b74bb6124dad1d5d49
V <lytranvinh.work@gmail.com>
2024-07-16 00:49:41 +0700

    [ADD]: Add loadding Factory Pattern ...

---
+++ CreationalPattern/FactoryPattern/Java/main.java
@@ -0,0 +1,11 @@
+/**
+ * @author: Ly Tran Vinh
+ * @contact: lytranvinh.work@gmail.com
+ * @content: Factory Pattern
+ */
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Factory Pattern");
+    }
+}
