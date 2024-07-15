10a642fedf05a508fb63d4671544a58838730b87
V <lytranvinh.work@gmail.com>
2024-07-16 02:54:10 +0700

    [ADD]: ADD abs factoty python file loading cmp !!!

--- CreationalPattern/AbstractFactoryPattern/Python/main.py
+++ CreationalPattern/AbstractFactoryPattern/Python/main.py
@@ -4,10 +4,36 @@
     @content: Abstract Factory Pattern
 """
 
+from enum import Enum
+
+class CharacterType(Enum):
+    """
+        Enum class to define the type of character
+    """
+    PLANT = "Plant"
+    ZOMBIE = "Zombie"
+
+class PlantType(Enum):
+    """
+        Enum class to define the type of plant
+    """
+    SUNFLOWER = "SunFlower"
+    PEASHOOTER = "PeaShooter"
+
+class ZombieType(Enum):
+    """
+        Enum class to define the type of zombie
+    """
+    NORMAL_ZOMBIE = "NormalZombie"
+    FLAG_ZOMBIE = "FlagZombie"
+
+# TODO cmp
+
 def main():
     """
         Main function to run the code
     """
+
 
 if __name__ == '__main__':
     main()
